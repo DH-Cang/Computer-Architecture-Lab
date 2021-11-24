c9fa5d134540e89e3af0269f6bb2a69e37bc1e14
zpqqq10 <2601087520@qq.com>
2021-11-24 15:20:25 +0800

    a little lab5

---
+++ lab5/code/core/FU_jump.v
@@ -0,0 +1,23 @@
+`timescale 1ns / 1ps
+
+module FU_jump(
+	input clk, EN, JALR,
+	input[2:0] cmp_ctrl,
+	input[31:0] rs1_data, rs2_data, imm, PC,
+	output[31:0] PC_jump, PC_wb,
+	output cmp_res, finish
+);
+
+    reg state;
+    assign finish = state == 1'b1;
+	initial begin
+        state = 0;
+    end
+
+	reg JALR_reg;
+	reg[2:0] cmp_ctrl_reg;
+	reg[31:0] rs1_data_reg, rs2_data_reg, imm_reg, PC_reg;
+
+	...            //to fill sth.in
+
+endmodule
